9cc2214471bcd31eddedb696027803e8bebf5ab0
Michal Wisniewski <mihuwis@gmail.com>
2019-08-14 21:36:33 +0200

    First commit

---
+++ src/main/java/com/progresspoint/gym_champion_exercises/GymChampionExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.progresspoint.gym_champion_exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymChampionExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymChampionExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/progresspoint/gym_champion_exercises/GymChampionExercisesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.progresspoint.gym_champion_exercises;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GymChampionExercisesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
